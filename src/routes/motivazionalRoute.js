f1ec20ccd9082b8a205432bd6f4d4e0b4677ee7b
lreale4125-ux <l.reale.4125@vallauri.edu>
2025-11-20 21:33:23 +0100

    Update motivazionalRoute.js

--- src/routes/motivazionalRoute.js
+++ src/routes/motivazionalRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const phrasesController = require('../controllers/phrasesController');
+const phrasesController = require('../controllers/n8nController');
 
 // POST /api/sync-phrases
 router.post('/sync-phrases', phrasesController.syncPhrases);
